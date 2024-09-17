098e9b20ddf7d32d09e3614fd4353226bc8606f2
MotSouzIan <ian_motasouza@hotmail.com>
2024-09-16 22:44:26 -0300

    primeiro commit

---
+++ EpidemicGuard/src/main/java/com/example/EpidemicGuard/EpidemicGuardApplication.java
@@ -0,0 +1,13 @@
+package com.example.EpidemicGuard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpidemicGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpidemicGuardApplication.class, args);
+	}
+
+}
---
+++ EpidemicGuard/src/test/java/com/example/EpidemicGuard/EpidemicGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EpidemicGuard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpidemicGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
