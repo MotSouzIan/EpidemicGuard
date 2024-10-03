861401d816b5ea625833cf04a044b475f743c131
Ian Mota <146491976+MotSouzIan@users.noreply.github.com>
2024-10-03 20:58:18 -0300

    Correção no AdministradorApplication

--- EpidemicGuard/src/main/java/com/example/EpidemicGuard/applications/AdministradorApplication.java
+++ EpidemicGuard/src/main/java/com/example/EpidemicGuard/applications/AdministradorApplication.java
@@ -3,48 +3,40 @@ package com.example.EpidemicGuard.applications;
 
 import com.example.EpidemicGuard.entities.Administrador;
 import com.example.EpidemicGuard.repositories.AdministradorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AdministradorApplication {
 
-    private AdministradorRepository administradorRepository;
+    private final AdministradorRepository administradorRepository;
 
+    @Autowired
     public AdministradorApplication(AdministradorRepository administradorRepository){
-
         this.administradorRepository = administradorRepository;
     }
 
     public Administrador buscarPorId(int id){
-
         return this.administradorRepository.buscarPorId(id);
-
     }
 
-    public ArrayList<Administrador> buscarTodos() {
-
+    public List<Administrador> buscarTodos() {
         return this.administradorRepository.buscarTodos();
     }
 
-    public void salvar(int id, String nome, String senha, String cpf) {
-
-        this.administradorRepository.salvar(id, nome, senha, cpf);
+    public void salvar(Administrador administrador) {
+        this.administradorRepository.salvar(administrador);
     }
 
     public void atualizar(int id, Administrador administrador) {
         this.administradorRepository.atualizar(id, administrador);
-
     }
 
     public void remover(int id) {
         this.administradorRepository.remover(id);
-
-    }
-
-    public void adicionar(Administrador administrador) {
-        this.administradorRepository.adicionar(administrador);
     }
 }
 
