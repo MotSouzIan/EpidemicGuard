50b40588e8a9025802eb57f55986d9ff780bcdc8
Ian Mota <146491976+MotSouzIan@users.noreply.github.com>
2024-10-03 21:00:08 -0300

    Correção no IAdministrador

---
+++ EpidemicGuard/src/main/java/com/example/EpidemicGuard/interfaces/IAdministrador.java
@@ -0,0 +1,19 @@
+package com.example.EpidemicGuard.interfaces;
+
+import com.example.EpidemicGuard.entities.Administrador;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface IAdministrador {
+
+    public Administrador buscarPorId(int id);
+
+    public List<Administrador> buscarTodos();
+
+    public void salvar(Administrador administrador);
+
+    public void atualizar(int id, Administrador administrador);
+
+    public void remover(int id);
+}
