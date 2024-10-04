9c891d7db2e91fc25a522c12fd282fe3753f5003
Ian Mota <146491976+MotSouzIan@users.noreply.github.com>
2024-10-03 21:01:22 -0300

    Correção no EpidemicGuardApplication

--- EpidemicGuard/src/main/java/com/example/EpidemicGuard/EpidemicGuardApplication.java
+++ EpidemicGuard/src/main/java/com/example/EpidemicGuard/EpidemicGuardApplication.java
@@ -4,20 +4,17 @@ import com.example.EpidemicGuard.facade.AdministradorFacade;
 import com.example.EpidemicGuard.facade.CidadaoFacade;
 import com.example.EpidemicGuard.entities.Administrador;
 import com.example.EpidemicGuard.entities.Cidadao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.util.Date;
 
 @SpringBootApplication
 public class EpidemicGuardApplication {
-
-	public EpidemicGuardApplication(){
-
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(EpidemicGuardApplication.class, args);
-
 	}
 }
